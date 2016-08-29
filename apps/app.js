ff028657eb162d00afb0646de04aecf722221913
Mattressburn <chevyboxer@gmail.com>
2016-08-29 15:17:39 -0400

    took out channel return from list

--- apps/app.js
+++ apps/app.js
@@ -1,28 +1,35 @@
 $(function(){
 
-	function thinktube(){
+	$('#search-form').submit(function(event) {
+		event.preventDefault();
+		var searchTerm = $('#query').val();
+		$('#query').val('');
+		thinktube(searchTerm)
+	})
+
+	function thinktube(searchTerm){
 		var params = {
 		part:'snippet',
 		key: 'AIzaSyAJdGOYvqYhVCnKQn2I_rfjVeAZqPvL3pA',
-		q: 'videogames'	
+		type: 'video',
+		q: searchTerm
 		};
 		var url = 'https://www.googleapis.com/youtube/v3/search'
 		$.getJSON(url, params , function(data){
-    	showResults(data)
+    	var searchResult = data.items
+			var html = ''
+			console.log(searchResult);
+			$.each(searchResult, function(index,value) {
+				var picture = value.snippet.thumbnails.medium.url,
+				link = 'https://www.youtube.com/watch?v=' + value.id.videoId;
+				html += '<p><a href ="' + link + '"></p> <br>' + '<p><img src="' + picture + '"></p><br>';
+				})
+			$('#search-results').html(html)
+
+
   		})
 	};
 
-	function showResults(results) {
-		var html = "";
-		$.each(results,function(index,value) {
-			html += '<p>' + value.Title + '</p>';
-			console.log(value.Title);
-		})
-		$('#search-results').html(html)
-	}
-
-	thinktube()
-	showResults(thinktube())
 });
 
 // ($(function() {
